019df49bc006d1474258adf84b71a8ad9bbd8fa6
Andrew Masih <armasih@outlook.com>
2021-05-02 18:28:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let tree = {
+  species: "oilve tree",
+  age: 150,
+  location: "greece", 
+    olives: {
+      amount: 1000,
+      color: "Black"
+    }
+};
+
+console.log (tree. olives. amount);
+console.log (tree. olives. color);
+
+/* The 'object constructor' method in the above example is the 3rd way to create an object . If you have an object that has another object as a property,then you can use 'dot notation' twice x2 to get the property of the inner object */
